101b3949913f942eddedc137f2dab11a8301525e
Naman12312 <namanthetoxic123@gmail.com>
2024-06-15 21:30:42 +0530

    g

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -12,14 +12,20 @@ int main(){
 	int cols1 = sizeof(arr1[0])/sizeof(int);
 	int rows2 = sizeof(arr2)/sizeof(arr2[0]);
 	int cols2 = sizeof(arr2[0])/sizeof(int);
-	int** arr0 = (int**)malloc(2*sizeof(int*));
+	/*
+	printf("rows1: %d\n",rows1);
+	printf("cols1: %d\n",cols1);
+	printf("rows2: %d\n",rows2);
+	printf("cols2: %d\n",cols2);
+	*/
+	int** arr0 = (int**)malloc(rows1*sizeof(int*));
 	for (int i = 0; i<2; i++) {
-		arr0[i] = (int*)malloc(3*sizeof(int));
+		arr0[i] = (int*)malloc(cols1*sizeof(int));
 	
 	}
-	int** arr00 = (int**)malloc(3*sizeof(int*));
+	int** arr00 = (int**)malloc(rows2*sizeof(int*));
 	for (int i = 0; i<3; i++) {
-		arr00[i] = (int*)malloc(2*sizeof(int));
+		arr00[i] = (int*)malloc(cols2*sizeof(int));
 	
 	}
 	
@@ -29,42 +35,55 @@ int main(){
 		goto end;
 	
 	}
-	for (int i = 0; i<2; i++) {
-		for (int j = 0; j<3; j++) {
+	for (int i = 0; i<rows1; i++) {
+		for (int j = 0; j<cols1; j++) {
 			arr0[i][j] = arr1[i][j];
 		
 		}
 	
 	}
-	for (int i = 0; i<3; i++) {
-		for (int j = 0; j<2; j++) {
+	for (int i = 0; i<rows2; i++) {
+		for (int j = 0; j<cols2; j++) {
 			arr00[i][j] = arr2[i][j];
 		
 		}
 	
 	}
-	int** arr00T = T(arr00, 3,2);
-	int** matMulArr = (int**)malloc(2*sizeof(int*));
+	int** arr00T = T(arr00, rows2,cols2);
+	printf("[");
+
+	for (int i = 0; i<rows1; i++) {
+		printf("[");
+		for (int j = 0; j<cols1; j++) {
+			printf("%d ", arr00T[i][j]);
+		
+		}
+		printf("]");
+		}
+	
+	}
+	printf("]");
+	int** matMulArr = (int**)malloc(rows1*sizeof(int*));
 	for (int i = 0; i<2; i++) {
-		matMulArr[i] = (int*)malloc(2*sizeof(int));
+		matMulArr[i] = (int*)malloc(cols2*sizeof(int));
 
 	
 	}
 	printf("Memory has been allocated for matMulArray\n");
-	for (int i = 0; i<2; i++) {
-		for (int j = 0; j<2; j++) {
-			matMulArr[i][j] = dotProduct(arr0[i], arr00T[j], 3);
+	for (int i = 0; i<rows1; i++) {
+		for (int j = 0; j<cols2; j++) {
+			matMulArr[i][j] = dotProduct(arr0[i], arr00T[j], rows1);
 		
 		}
 	
 	}
-
+	/*
 
 	printf("[");
 
-	for (int i = 0; i<2; i++) {
+	for (int i = 0; i<rows1; i++) {
 		printf("[");
-		for (int j = 0; j<2; j++) {
+		for (int j = 0; j<cols2; j++) {
 			printf((j!=1)?"%d ":"%d", matMulArr[i][j]);
 		
 		}
@@ -80,7 +99,7 @@ int main(){
 
 
 
-
+*/
 
 
 
